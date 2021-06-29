f4622b47a73259ff7a96cf95ab30d26769bb8f37
d <d@DESKTOP-7950S96>
2021-06-29 11:59:37 +0530

    First commit...

---
+++ src/defuat/Test1.java
@@ -0,0 +1,26 @@
+package defuat;
+import java.util.ArrayList;
+public class Test1 {
+
+	public static void main(String[] args) 
+	{
+        ArrayList list=new ArrayList();
+        list.add("Rahul");
+        list.add("yogesh");
+        list.add("Sagar");
+        list.add("Sandesh");
+        list.add(10);
+        System.out.println(list.size());
+        System.out.println(list);
+        System.out.println(list.remove(2));
+        System.out.println(list);
+        list.add(3,"ramesh");
+        System.out.println(list);
+        
+        
+        
+        	
+        
+	}
+
+}
